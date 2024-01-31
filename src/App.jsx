f1698e6dfb1076ee9c8111a3c82f192fe8568416
Mr. Sanjoy <--add>
2024-01-31 10:53:08 +0530

    increment decrement state is updating on buttons click

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import store from './app/store';
+import { increment, decrement } from './features/counter/counterSlice';
 
 const App = () => {
+
+  console.log(store.getState());
+  const value = useSelector(state => state.counter.value);
+  const diapatch = useDispatch();
+
 
   function handleClick(event) {
     switch (event.target.id) {
       case 'increment': {
         console.log(event.target.id);
+        diapatch(increment())
       }
         break;
       case 'decrement': {
         console.log(event.target.id);
+        diapatch(decrement())
       }
         break;
       default:
@@ -22,7 +32,7 @@ const App = () => {
     <div className=''>
 
       <h1 className='text-xl '>Redux React Vite App</h1>
-      <h1 className='font-bold text-2xl text-blue-700 m-2'>Value : {''} </h1>
+      <h1 className='font-bold text-2xl text-blue-700 m-2'>Value : {value} </h1>
       <button className='bg-green-400 rounded p-2 font-semibold m-2' id='increment' onClick={handleClick}>Increment</button> <br />
       <button className='bg-red-600 rounded p-2 font-semibold m-2' id='decrement' onClick={handleClick}>Decrement</button>
 
