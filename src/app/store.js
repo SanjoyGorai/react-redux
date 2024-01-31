2ef795e880267dce2e52c227666cce943e55d373
Mr. Sanjoy <--add>
2024-01-31 10:45:12 +0530

    counter slice is created

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
 
 import { configureStore } from '@reduxjs/toolkit';
-
+import counterReducer from '../features/counter/counterSlice'
 
 const store = configureStore({
     reducer: {
-        counter: ''
+        counter: counterReducer
     }
 });
 
